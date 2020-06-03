7a123649f65abc63bbaa421ca506c53a482e2506
angelo <angelo.pjsol@gmail.com>
2020-06-02 22:52:54 -0300

    menu adjusts

--- sistema/frontend/src/components/menu/index.js
+++ sistema/frontend/src/components/menu/index.js
@@ -1,37 +1,43 @@
-<header>
-    <nav className="menuBlock">
-        <div className="userBlock">
-            <div className="userBox">
-                <img src="# " />
-                <p>Admin</p>
-            </div>
-        </div>
-        <hr />
-        <div className="navBlock">
-            <div className="navBox">
-                <img src="#" />
-                <p>cadastrar</p>
-            </div>
-            <div className="navBox">
-                <img src="#" />
-                <p>nova movimentação</p>
-                <div class="navSubBox">
-                    <img src="#" />
-                    <p>movimentação interna</p>
-                </div>
-                <div class="navSubBox">
-                    <img src="#" />
-                    <p>movimentação interna</p>
-                </div>
-                <div class="navSubBox">
-                    <img src="#" />
-                    <p>movimentação interna</p>
+import React, { useEffect, useState } from 'react'
+
+
+export default function Menu() {
+return (
+    <header>
+        <nav className="menuBlock">
+            <div className="userBlock">
+                <div className="userBox">
+                    <img src="# " />
+                    <p>Admin</p>
                 </div>
             </div>
-            <div className="navBox">
-                <img src="#" />
-                <p>Registros</p>
+            <hr />
+            <div className="navBlock">
+                <div className="navBox">
+                    <img src="#" />
+                    <p>cadastrar</p>
+                </div>
+                <div className="navBox">
+                    <img src="#" />
+                    <p>nova movimentação</p>
+                    <div class="navSubBox">
+                        <img src="#" />
+                        <p>movimentação interna</p>
+                    </div>
+                    <div class="navSubBox">
+                        <img src="#" />
+                        <p>movimentação interna</p>
+                    </div>
+                    <div class="navSubBox">
+                        <img src="#" />
+                        <p>movimentação interna</p>
+                    </div>
+                </div>
+                <div className="navBox">
+                    <img src="#" />
+                    <p>Registros</p>
+                </div>
             </div>
-        </div>
-    </nav>
-</header>
+        </nav>
+    </header>
+)};
