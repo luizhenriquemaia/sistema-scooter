2ee777007043702933b82669b9d288bc12b647b4
angelo <angelo.pjsol@gmail.com>
2020-06-02 22:48:27 -0300

    menu index.js

---
+++ sistema/frontend/src/components/menu/index.js
@@ -0,0 +1,37 @@
+<header>
+    <nav className="menuBlock">
+        <div className="userBlock">
+            <div className="userBox">
+                <img src="# " />
+                <p>Admin</p>
+            </div>
+        </div>
+        <hr />
+        <div className="navBlock">
+            <div className="navBox">
+                <img src="#" />
+                <p>cadastrar</p>
+            </div>
+            <div className="navBox">
+                <img src="#" />
+                <p>nova movimentação</p>
+                <div class="navSubBox">
+                    <img src="#" />
+                    <p>movimentação interna</p>
+                </div>
+                <div class="navSubBox">
+                    <img src="#" />
+                    <p>movimentação interna</p>
+                </div>
+                <div class="navSubBox">
+                    <img src="#" />
+                    <p>movimentação interna</p>
+                </div>
+            </div>
+            <div className="navBox">
+                <img src="#" />
+                <p>Registros</p>
+            </div>
+        </div>
+    </nav>
+</header>
