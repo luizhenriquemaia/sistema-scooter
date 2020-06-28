6c982db105a128a60b661d905f4d7dd631ff0f2e
LuizHenriqueMaia <luizhenriquembh@gmail.com>
2020-06-28 03:29:28 -0300

    make routes to api for bases and employees

--- sistema/main/urls.py
+++ sistema/main/urls.py
@@ -1,10 +1,17 @@
-from django.urls import path
 from django.conf.urls import include
+from django.urls import path
 from rest_framework import routers
-from .api import StatusScooterViewSet, TypeMovementViewSet, LogisticOperatorViewSet, ScooterViewSet, PeopleRegistrationViewSet, MovementViewSet
 
+from .api import (BaseOfWorkViewSet, EmployeeSerializer,
+                  LogisticOperatorViewSet, MovementViewSet,
+                  PeopleRegistrationViewSet, ScooterViewSet,
+                  StatusScooterViewSet, TypeMovementViewSet)
 
 router = routers.DefaultRouter()
+router.register('api/base-of-work', 
+                BaseOfWorkViewSet, 'base-of-work')
+router.register('api/employee',
+                EmployeeSerializer, 'employee')
 router.register('api/status-scooter', 
                 StatusScooterViewSet, 'status-scooter')
 router.register('api/type-movement',
@@ -15,11 +22,5 @@ router.register('api/scooter',
                 ScooterViewSet, 'scooter')
 router.register('api/people-registration', PeopleRegistrationViewSet, 'peopleRegistration')
 router.register('api/movement', MovementViewSet, 'movement')
-
-
-
-urlpatterns = [
-    #path('api/auth', include('rest_framework.urls')),
-]
 
 urlpatterns += router.urls
