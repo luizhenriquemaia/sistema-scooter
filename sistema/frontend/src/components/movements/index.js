c06216d5bb963b46adf3a07e4112a52b1b873567
LuizHenriqueMaia <luizhenriquembh@gmail.com>
2020-07-09 10:01:57 -0300

    see if scooter was added before map, error of map object

--- sistema/frontend/src/components/movements/index.js
+++ sistema/frontend/src/components/movements/index.js
@@ -185,37 +185,39 @@ export default function Movements() {
     }, [movements])
 
     useEffect(() => {
-        if (scooters !== undefined) {
-            if (scooters.length !== 0) {
-                let numberOfScooters = 0
-                let numberOfScootersInUse = 0
-                let numberOfScootersAvailable = 0
-                let numberOfScootersUnderMaintenance = 0
-                let numberOfScootersOperants = 0
-                let numberOfScootersInBackup = 0
-                if (scooters.map) {
-                    scooters.map(scooter => {
-                        if (scooter.status.description === "Em uso") {
-                            numberOfScootersInUse += 1
-                            numberOfScootersOperants += 1
-                        } 
-                        if (scooter.status.description === "Disponível") {
-                            numberOfScootersAvailable += 1
-                            numberOfScootersOperants += 1
-                        }
-                        if (scooter.status.description === "Manutenção") numberOfScootersUnderMaintenance += 1
-                        if (scooter.status.description === "Backup") numberOfScootersInBackup += 1
-                        numberOfScooters += 1
-                    })
-                    setNumbersOfScootersState({
-                        numberOfScooters,
-                        numberOfScootersInUse,
-                        numberOfScootersAvailable,
-                        numberOfScootersUnderMaintenance,
-                        numberOfScootersOperants,
-                        numberOfScootersInBackup
-                    })
-                    setScootersState(scooters)
+        if (!wasScooterAdded) {
+            if (scooters !== undefined) {
+                if (scooters.length !== 0) {
+                    let numberOfScooters = 0
+                    let numberOfScootersInUse = 0
+                    let numberOfScootersAvailable = 0
+                    let numberOfScootersUnderMaintenance = 0
+                    let numberOfScootersOperants = 0
+                    let numberOfScootersInBackup = 0
+                    if (scooters.length > 0) {
+                        scooters.map(scooter => {
+                            if (scooter.status.description === "Em uso") {
+                                numberOfScootersInUse += 1
+                                numberOfScootersOperants += 1
+                            } 
+                            if (scooter.status.description === "Disponível") {
+                                numberOfScootersAvailable += 1
+                                numberOfScootersOperants += 1
+                            }
+                            if (scooter.status.description === "Manutenção") numberOfScootersUnderMaintenance += 1
+                            if (scooter.status.description === "Backup") numberOfScootersInBackup += 1
+                            numberOfScooters += 1
+                        })
+                        setNumbersOfScootersState({
+                            numberOfScooters,
+                            numberOfScootersInUse,
+                            numberOfScootersAvailable,
+                            numberOfScootersUnderMaintenance,
+                            numberOfScootersOperants,
+                            numberOfScootersInBackup
+                        })
+                        setScootersState(scooters)
+                    }
                 }
             }
         }
