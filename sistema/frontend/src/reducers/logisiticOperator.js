b9cb43e2183a8773a74bcdcaf92da281f1d2c85d
LuizHenriqueMaia <luizhenriquembh@gmail.com>
2020-07-09 09:48:59 -0300

    fix bug of first LO added in system

--- sistema/frontend/src/reducers/logisiticOperator.js
+++ sistema/frontend/src/reducers/logisiticOperator.js
@@ -3,20 +3,23 @@ import { GET_LOGISTICS_OPERATORS, ADD_LOGISTIC_OPERATOR } from '../actions/types
 
 
 const initialState = {
-    logisticOperator: []
+    logisticOperator: [],
+    wasAdded: false
 }
-
+ 
 export default function (state = initialState, action) {
     switch (action.type) {
         case GET_LOGISTICS_OPERATORS:
             return {
                 ...state,
                 logisticOperator: action.payload,
+                wasAdded: false
             }
         case ADD_LOGISTIC_OPERATOR:
             return {
                 ...state,
-                logisticOperator: [...state.logisticOperator, action.payload]
+                logisticOperator: action.payload,
+                wasAdded: true
             }
         default:
             return state
